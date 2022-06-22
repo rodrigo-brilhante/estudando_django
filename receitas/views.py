f81e592f3c5fbb460dbdb4a4fe28ff2d324c4068
rodrigo-brilhante <rodrigo.abb@outlook.com>
2022-06-22 11:38:09 -0300

    primeiro commit

---
+++ receitas/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import get_object_or_404, render
+from .models import Receita
+
+def index(request):
+    receitas = Receita.objects.all()
+
+    dados = {
+        'receitas' : receitas
+    }
+    return render(request, 'index.html', dados)
+
+def receita(request, receita_id):
+    receita = get_object_or_404(Receita, pk=receita_id)
+
+    dados = {
+        'receita':receita
+    }
+    return render(request, 'receita.html', dados)
+
